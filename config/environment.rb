89686f806b573098526a6923329184ccbfcaf508
Tony Xie <tx248@nyu.edu>
2016-11-01 14:52:25 -0700

    update production.rb for assets on heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.serve_static_files = true
+# ...
+config.log_level = :debug
