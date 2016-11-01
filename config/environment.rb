435b8a9eb3d396eee39db8efa650e69c5b49f4bb
Tony Xie <tx248@nyu.edu>
2016-11-01 15:21:03 -0700

    3rd try to heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-config.serve_static_files = true
-# ...
-config.log_level = :debug
